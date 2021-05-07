56f57f103d9d141a8912157af0782c98a3741eb1
Aditya Vaidyam <avaidyam@users.noreply.github.com>
2021-05-07 13:39:09 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import os 
+import requests  
+records = requests.post(os.getenv('API_URL', ''), data={
+  'token': os.getenv('API_TOKEN', ''),
+  'content': 'record',
+  'format': 'json',
+  'type': 'flat',
+  'exportSurveyFields': 'true'
+}).json()
