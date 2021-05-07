f5532af3597492f13baf1529fac282e0d7ec4101
Aditya Vaidyam <avaidyam@users.noreply.github.com>
2021-05-07 13:48:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import os 
-import requests  
-records = requests.post(os.getenv('API_URL', ''), data={
-  'token': os.getenv('API_TOKEN', ''),
-  'content': 'record',
-  'format': 'json',
-  'type': 'flat',
-  'exportSurveyFields': 'true'
-}).json()
-print(records[0:2])
+import LAMP
+import requests
+LAMP.connect()
+count = 0
+for researcher in LAMP.Researcher.all()['data']:
+    try:
+        config = LAMP.Type.get_attachment(researcher['id'], 'org.digitalpsych.redcap.importer')['data']
+        count += 1
+    except LAMP.ApiException:
+        continue
+    fields = {
+        'token': config['API_TOKEN'],
+        'content': 'record',
+        'format': 'json',
+        'type': 'flat',
+        'exportSurveyFields': 'true'
+    }
+    records = requests.post(config['API_URL'], data=fields).json()
+    LAMP.Type.set_attachment(researcher['id'], 'me',
+                             attachment_key='org.digitalpsych.redcap.data',
+                             body=records)
+    print(count)
