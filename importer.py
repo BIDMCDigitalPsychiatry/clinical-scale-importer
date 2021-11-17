c7b448fc791cff750a0f0e9455e5cdd944a29429
dcurrey88 <dcurrey8@gmail.com>
2021-11-17 11:07:57 -0500

    making requested updates

--- importer.py
+++ importer.py
@@ -21,37 +21,34 @@ for researcher in LAMP.Researcher.all()['data']:
                              attachment_key='org.digitalpsych.redcap.data',
                              body=records)
     print(count)
-    try:
-        arr = LAMP.Type.get_attachment(researcher['id'], 'org.digitalpsych.redcap.links')["data"]
-    except LAMP.ApiException:
-        continue # Ignore this Researcher
-    parts = []
-    for study in LAMP.Study.all_by_researcher(researcher['id'])['data']:
-        parts+=(p['id'] for p in LAMP.Participant.all_by_study(study['id'])['data'])
-    for p in parts:
-        try:
-            record_id = LAMP.Type.get_attachment(p, 'org.digitalpsych.redcap.id')["data"]
-        except LAMP.ApiException:
-            continue # Ignore this Participant
-        if record_id >= 0:
+    if "IMPORT_SHARE_LINKS" in config:
+        parts = []
+        for study in LAMP.Study.all_by_researcher(researcher['id'])['data']:
+            parts+=(p['id'] for p in LAMP.Participant.all_by_study(study['id'])['data'])
+        for p in parts:
             try:
-                link = LAMP.Type.get_attachment(p, 'org.digitalpsych.redcap.links')["data"]
+                record_id = LAMP.Type.get_attachment(p, 'org.digitalpsych.redcap.id')["data"]
             except LAMP.ApiException:
-                links = {}
-                for s in arr:
-                    data = {
-                        'token': config['API_TOKEN'],
-                        'content': 'surveyLink',
-                        'format': 'json',
-                        'instrument': s,
-                        'event': '',
-                        'record': record_id,
-                        'returnFormat': 'json'
-                    }
-                    r = requests.post(config['API_URL'], data=data)
-                    links[s] = r.text
-                LAMP.Type.set_attachment(p, 'me',
-                                attachment_key='org.digitalpsych.redcap.links',
-                                body=links)
+                continue # Ignore this Participant
+            if record_id >= 0:
+                try:
+                    link = LAMP.Type.get_attachment(p, 'org.digitalpsych.redcap.share_links')["data"]
+                except LAMP.ApiException:
+                    links = {}
+                    for s in config["IMPORT_SHARE_LINKS"]:
+                        data = {
+                            'token': config['API_TOKEN'],
+                            'content': 'surveyLink',
+                            'format': 'json',
+                            'instrument': s,
+                            'event': '',
+                            'record': record_id,
+                            'returnFormat': 'json'
+                        }
+                        r = requests.post(config['API_URL'], data=data)
+                        links[s] = r.text
+                    LAMP.Type.set_attachment(p, 'me',
+                                    attachment_key='org.digitalpsych.redcap.share_links',
+                                    body=links)
 
 print("Finished importing data.")
